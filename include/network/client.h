372d70a832a9e367ce37986a7bde9445b92c131a
artemuch12 <ivashka35-45@yandex.ru>
2019-11-24 17:25:58 +0700

    Void

--- include/network/client.h
+++ include/network/client.h
@@ -1,8 +1,9 @@
 #ifndef _CLIENT_H_
 #define _CLIENT_H_
-
+#icnlude "../logic/game.h"
 void init_client(int, char **);
-int reception();
+void reception();
 void expectation();
-
+void recv_message(struct game *, struct enemy *, struct user *, struct bullet *)
+void send_message(int, int, void *)
 #endif // _CLIENT_H_
--- src/network/client.c
+++ src/network/client.c
@@ -10,20 +10,34 @@
 #include <netinet/udp.h>
 #include <arpa/inet.h>
 #include "../../include/network/client.h"
+#include "../../include/logic/game.h"
 
 struct serv_information {
   unsigned int status;
 };
+struct message_transmitting{
+  unsigned int status;
+  unsigned int id_user;
+  void *data;
+};
+/*Теги для status, 1-ая группа технические*/
+#define CONNECT 1
+#define STRT_GS 2
+#define ERR_CONN 3
+#define END_GS 9
+/*2-ая, группа игровой направлености*/
+#define MV_LEFT 30
+#define MV_RIGHT 31
 
 struct sockaddr_in addr_server;
 struct serv_information information_to_player;
 struct serv_information information_from_player;
-
+struct message_transmitting message;
 socklen_t addr_in_size = sizeof(struct sockaddr_in);
 pthread_t receiver_from_server;
 
 int file_descrip_client;
-
+int id;
 void init_client(int argc, char **argv)
 {
   addr_server.sin_family = AF_INET;
@@ -40,10 +54,10 @@ void init_client(int argc, char **argv)
   }
 }
 
-int reception()
+void reception()
 {
   file_descrip_client = socket(AF_INET, SOCK_DGRAM, 0);
-  information_from_player.status = 1;
+  information_from_player.status = CONNECT;
 
   sendto(file_descrip_client, &information_from_player,
          sizeof(information_from_player), 0,
@@ -52,8 +66,6 @@ int reception()
   recvfrom(file_descrip_client, &information_to_player,
            sizeof(information_to_player), 0,
            (struct sockaddr *)&addr_server, &addr_in_size);
-
-  return information_to_player.status;
 }
 
 void *receiver() /*Заготовка*/
@@ -80,12 +92,19 @@ void *receiver() /*Заготовка*/
 
 void expectation()
 {
+  struct game game_ses;
+  unsigned int id;
   int flag = 1;
+
+  recv_message(game_ses, NULL, NULL, NULL);
+  id = message.id_user;
+
   recvfrom(file_descrip_client, &information_to_player,
            sizeof(information_to_player), 0,
            (struct sockaddr *)&addr_server, &addr_in_size);
 
-  if (information_to_player.status == 2) {
+  if (information_to_player.status == STRT_GS) {
+
     //game_session();
   } else {
     while (flag == 1) {
@@ -93,11 +112,39 @@ void expectation()
                sizeof(information_to_player), 0,
                (struct sockaddr *)&addr_server, &addr_in_size);
 
-      if (information_to_player.status == 2) {
+      if (information_to_player.status == STRT_GS) {
         flag = 0;
       }
     }
 
     //game_session();
+  }
+}
+void send_message(int status, int id_user, void *data)
+{
+  message.status = status;
+  message.id_user = id_user;
+  message.data = &data;
+  sendto(file_descrip_client, &message,
+         sizeof(message), 0,
+         (struct sockaddr *)&addr_server, &addr_in_size);/*надо посмотреть id_user*/
+}
+void recv_message(struct game *game_mess, struct enemy * enemy_mess, struct user *user_mess, struct bullet * bullet__mess)
+{
+  recvfrom(file_descrip_server, &message,
+         sizeof(message), 0,
+         (struct sockaddr *)&addr_server, addr_in_size);
+  switch(message.status)
+  {
+    case: STRT_GS
+    game_mess  = (struct game *)message.data;
+    break;
+    case: MV_LEFT
+
+    break;
+    case: MV_RIGHT
+
+    break;
+    /*И другие*/
   }
 }
